5e266764d8fe239264c12eacdf374c3bd1f80537
Fedor Kosolapov <fedor_coder@mail.ru>
2020-05-20 05:19:22 +0700

    move working with tmp-file to module

--- huntflow.py
+++ huntflow.py
@@ -1,36 +1,15 @@
 ''' Скрипт для загрузки данных о кандидатах из xls-файла
     в базу данных huntflow.
 '''
-
 import os
 
 from modules.api import send_applicant
 from modules.api import REQUEST_SUCCESS, REQUEST_ERROR
 from modules.excel import get_applicant_from_xls_file
 from modules.args import get_command_line_args
-
-
-TMP_FILENAME = os.path.dirname(os.path.abspath(__file__)) + '/err_strings.tmp'
-
-
-def write_to_file_str_number(str_number):
-    ''' Сохраняет ошибочные позиции во временный файл '''
-    mode = 'a'
-    if not os.path.exists(TMP_FILENAME):
-        mode = 'w'
-    with open(TMP_FILENAME, mode) as file:
-        file.write(str(str_number)+'\n')
-
-def get_str_numbers_list_from_file():
-    ''' Получает ошибочные позиции из временного файла '''
-    str_numbers_list = list()
-    if not os.path.exists(TMP_FILENAME):
-        return None
-    with open(TMP_FILENAME, 'r') as file:
-        for line in file:
-            str_numbers_list.append(int(line))
-    os.remove(TMP_FILENAME)
-    return str_numbers_list
+from modules.tmp import get_str_numbers_list_from_file, write_to_file_str_number
+from modules.tmp import write_to_file_str_number
+from modules.tmp import TMP_FILENAME
 
 
 if __name__ == "__main__":
---
+++ modules/tmp.py
@@ -0,0 +1,24 @@
+import os
+
+
+TMP_FILENAME = os.path.dirname(os.path.abspath(__file__)) + '/err_strings.tmp'
+
+
+def write_to_file_str_number(str_number):
+    ''' Сохраняет ошибочные позиции во временный файл '''
+    mode = 'a'
+    if not os.path.exists(TMP_FILENAME):
+        mode = 'w'
+    with open(TMP_FILENAME, mode) as file:
+        file.write(str(str_number)+'\n')
+
+def get_str_numbers_list_from_file():
+    ''' Получает ошибочные позиции из временного файла '''
+    str_numbers_list = list()
+    if not os.path.exists(TMP_FILENAME):
+        return None
+    with open(TMP_FILENAME, 'r') as file:
+        for line in file:
+            str_numbers_list.append(int(line))
+    os.remove(TMP_FILENAME)
+    return str_numbers_list
